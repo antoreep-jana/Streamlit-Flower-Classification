db4617d6925c0d1d634d5bf264093ede413e9655
Antoreep Jana <antoreepjana@gmail.com>
2021-07-27 07:55:57 +0530

    new commit

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 import streamlit as st 
 import tensorflow as tf 
 
+!apt update
+!apt-get install -y libglib2.0-0 libsm6 libxrender1 libxext6
+
 
 st.set_option('deprecation.showfileUploaderEncoding', False)
 
